8201e8916a83af7766314c52be63371e89e8d514
Leandro Heck <leoheck@gmail.com>
2023-04-10 13:51:59 -0300

    Remove inid files

--- startntripserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# $Id: startntripserver.sh,v 1.2 2007/08/30 15:02:19 stuerze Exp $
-# Purpose: Start ntripserver
-
-while true; do ./ntripserver -M 1 -i /dev/ttys0 -b 9600 -O 2 -a www.euref-ip.net -p 2101 -m Mount2 -n serverID -c serverPass; sleep 60; done
